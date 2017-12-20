689ac6fb75836622e58832676f3d532f8c880842
tianyongxu <tianyongxubz@163.com>
2017-12-20 19:46:18 +0800

    today's homework

---
+++ src/main/java/dt59haomework1/woek2.java
@@ -0,0 +1,28 @@
+/**
+ * Project Name:dt59haomework
+ * File Name:woek2.java
+ * Package Name:dt59haomework1
+ * Date:2017年12月20日下午7:44:23
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59haomework1;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:44:23 <br/>
+ * @author   A555l
+ * @version
+ * @see
+ */
+public class woek2 {
+
+    public static void main(String[] args) {
+
+       int a = 12;
+       String ary = "tianyongxu";
+       System.out.println(a+ary);
+
+    }
+
+}
+
