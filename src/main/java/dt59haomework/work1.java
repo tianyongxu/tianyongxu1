65235bf447a0505efa508aa7e3ff274285e1dd03
tianyongxu <tianyongxubz@163.com>
2017-12-20 19:33:07 +0800

    today's homework

---
+++ src/main/java/dt59haomework/work1.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:dt59haomework
+ * File Name:work1.java
+ * Package Name:dt59haomework
+ * Date:2017年12月20日下午7:05:30
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59haomework;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:05:30 <br/>
+ * @author   A555l
+ * @version
+ * @see
+ */
+public class work1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello word!");
+    }
+
+}
+
